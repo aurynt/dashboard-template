63fe55023eb8eff27dfa3586e64e1293eb14c8f0
riyant <anggurryn106@gmail.com>
2024-03-05 11:12:51 +0700

    add:Input password comp

--- src/components/InputPassword.tsx
+++ src/components/InputPassword.tsx
@@ -1,55 +1,45 @@
 "use client";
-import React, { HtmlHTMLAttributes, useState } from "react";
-import { Input } from "./ui/input";
+import { InputHTMLAttributes, forwardRef, useState } from "react";
 import { LuEye, LuEyeOff } from "react-icons/lu";
-
-export default function InputPassword(
-  props: HtmlHTMLAttributes<HTMLInputElement>
-) {
-  const [show, setShow] = useState(false);
-  const handleClick = () => {
-    setShow(!show);
-  };
-  return (
-    <div className="relative w-full">
-      {show ? (
-        <LuEye
-          onClick={handleClick}
-          className="absolute right-2 top-2 cursor-pointer"
-          size={20}
-        />
-      ) : (
-        <LuEyeOff
-          onClick={handleClick}
-          className="absolute right-2 top-2 cursor-pointer"
-          size={20}
-        />
-      )}
-      <Input type={show ? "text" : "password"} {...props} />
-    </div>
-  );
-}
 
 import { cn } from "@/lib/utils";
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
+const InputPassword = forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const [show, setShow] = useState(false);
+    const handleClick = () => {
+      setShow(!show);
+    };
     return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus:bg-muted/10 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
-          className
+      <div className="relative w-full">
+        {show ? (
+          <LuEye
+            onClick={handleClick}
+            className="absolute right-2 top-2 cursor-pointer"
+            size={20}
+          />
+        ) : (
+          <LuEyeOff
+            onClick={handleClick}
+            className="absolute right-2 top-2 cursor-pointer"
+            size={20}
+          />
         )}
-        ref={ref}
-        {...props}
-      />
+        <input
+          type={show ? "text" : "password"}
+          className={cn(
+            "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus:bg-muted/10 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+      </div>
     );
   }
 );
-Input.displayName = "Input";
+InputPassword.displayName = "InputPassword";
 
-export { Input };
+export { InputPassword };
