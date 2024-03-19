eb95ed6df5324a2ec299ae9e23e1d8d179e500a4
riyant <anggurryn106@gmail.com>
2024-03-19 11:09:26 +0700

    style:Dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,36 +4,38 @@ import { countPropertyFromArray, formatCurrency } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between backdrop-brightness-100">
-      <div className="flex sm:flex-row flex-col gap-4 sm:gap-8 mb-8">
-        <div className="rounded-lg border p-3 h-24 w-full relative">
-          <h1 className="font-bold uppercase">Product</h1>
-          <span className="text-3xl absolute font-bold text-teal bottom-3 right-6">
-            {product.length}
-          </span>
+    <main className="flex sm:flex-row flex-col gap-5 justify-between backdrop-brightness-100">
+      <div className="flex flex-col">
+        <div className="flex w-full sm:flex-row flex-col gap-5 mb-8">
+          <div className="rounded-lg border p-3 h-24 w-full relative">
+            <h1 className="font-bold capitalize">Product</h1>
+            <span className="text-3xl absolute font-bold text-teal bottom-3 right-6">
+              {product.length}
+            </span>
+          </div>
+          <div className="rounded-lg border p-3 h-24 w-full relative">
+            <h1 className="font-bold capitalize">Stock</h1>
+            <span className="text-3xl absolute font-bold text-teal bottom-3 right-6 capitalize">
+              {countPropertyFromArray(product, "stock")} pcs
+            </span>
+          </div>
+          <div className="rounded-lg border p-3 h-24 w-full relative">
+            <h1 className="font-bold capitalize">Revenue</h1>
+            <span className="text-xl absolute font-bold text-teal bottom-3 right-6">
+              {formatCurrency(countPropertyFromArray(product, "price"))}
+            </span>
+          </div>
         </div>
-        <div className="rounded-lg border p-3 h-24 w-full relative">
-          <h1 className="font-bold uppercase">Stock</h1>
-          <span className="text-3xl absolute font-bold text-teal bottom-3 right-6 uppercase">
-            {countPropertyFromArray(product, "stock")} pcs
-          </span>
-        </div>
-        <div className="rounded-lg border p-3 h-24 w-full relative">
-          <h1 className="font-bold uppercase">Income</h1>
-          <span className="text-3xl absolute font-bold text-teal bottom-3 right-6">
-            {formatCurrency(countPropertyFromArray(product, "price"))}
-          </span>
-        </div>
+        <h1>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Exercitationem accusantium inventore consectetur dignissimos hic
+          tenetur nam magnam quas cumque, nulla ratione, sed ullam illum saepe.
+          Nostrum nulla totam rem vero.
+        </h1>
       </div>
-      <div className="h-72 p-3 sm:w-[25rem] border backdrop-hue-rotate-30 w-full rounded-lg mb-8">
+      <div className="h-72 p-3 sm:w-[25rem] min-w-72 border backdrop-hue-rotate-30 w-full rounded-lg mb-8">
         <Chart data={product} dataKey="price" />
       </div>
-      <h1>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, magni?
-        Quae consectetur, alias dolorum eum commodi facilis expedita laborum,
-        fugiat ab iure enim labore veritatis rem voluptatem perspiciatis at
-        quisquam!
-      </h1>
     </main>
   );
 }
